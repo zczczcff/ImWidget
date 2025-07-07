244f18e342a9e8f3db3f01c89ea574c88dfd3f68
zc_108 <1649319507@qq.com>
2025-07-07 20:43:46 +0800

    button 事件回调，style测试成功

--- Test/Test0/Test_ButtonTest.cpp
+++ Test/Test0/Test_ButtonTest.cpp
@@ -17,14 +17,39 @@ public:
     void Init()
     {
         m_Canvas = new ImGuiWidget::ImCanvasPanel("Canvas0");
+        ImGuiWidget::ImTextBlock* ButtonTestDisplayText = new ImGuiWidget::ImTextBlock("ButtonTestDisplayText");
         ImGuiWidget::ImButton* button0 = new ImGuiWidget::ImButton("button0");
         ImGuiWidget::ImButton* button1 = new ImGuiWidget::ImButton("button1");
         ImGuiWidget::ImButton* button2 = new ImGuiWidget::ImButton("button2");
-
+        button0->SetOnPressed([ButtonTestDisplayText]() 
+            {
+                ButtonTestDisplayText->SetText("button0pressed");
+            });
+        button0->SetOnReleased([ButtonTestDisplayText]() 
+            {
+                ButtonTestDisplayText->SetText("");
+            });
+        button1->SetOnPressed([ButtonTestDisplayText]()
+            {
+                ButtonTestDisplayText->SetText("button1pressed");
+            });
+        button1->SetOnReleased([ButtonTestDisplayText]()
+            {
+                ButtonTestDisplayText->SetText("");
+            });
+        button2->SetOnPressed([ButtonTestDisplayText]()
+            {
+                ButtonTestDisplayText->SetText("button2pressed");
+            });
+        button2->SetOnReleased([ButtonTestDisplayText]()
+            {
+                ButtonTestDisplayText->SetText("");
+            });
         //m_Canvas->AddChildToCanvasPanel(ResizableBox0)->SetSlotPosAndSize(ImVec2(400, 400), ImVec2(400, 400));
         m_Canvas->AddChildToCanvasPanel(button0)->SetSlotPosAndSize(ImVec2(10, 10), ImVec2(100, 20));
         m_Canvas->AddChildToCanvasPanel(button1)->SetSlotPosAndSize(ImVec2(10, 32), ImVec2(100, 20));
         m_Canvas->AddChildToCanvasPanel(button2)->SetSlotPosAndSize(ImVec2(10, 54), ImVec2(100, 20));
+        m_Canvas->AddChildToCanvasPanel(ButtonTestDisplayText)->SetSlotPosAndSize(ImVec2(10, 76), ImVec2(100, 20));
     }
     void Render() override
     {
