ff07734980b52912cbb79e67b0947230b3f3a4c7
zc_108 <1649319507@qq.com>
2025-08-13 23:30:38 +0800

    8.13

--- ImWidget/ImWidget/ImBorder.h
+++ ImWidget/ImWidget/ImBorder.h
@@ -11,5 +11,20 @@ namespace ImGuiWidget
 		{
 			return new ImPaddingSlot(Content);
 		}
+
+		virtual ImSlot* AddChild(ImWidget* child, ImVec2 RelativePosition = ImVec2(FLT_MIN, FLT_MIN))
+		{
+			if (GetSlotNum() >= 1)
+			{
+				return nullptr;
+			}
+
+			return AddChildInternal<ImPaddingSlot>(child);
+		}
+
+		void SetContent(ImWidget* Content)
+		{
+			SetChildAt(0, Content);
+		}
 	};
 }
